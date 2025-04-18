a781997d9afa06ba64127a5557fca23200f64f17
Fadonougbo <fadougbogautier@gmail.com>
2025-04-18 19:54:22 +0100

    updaetalpha

---
+++ start/routes.js
@@ -0,0 +1,5 @@
+import logger from '@adonisjs/core/services/logger';
+import router from '@adonisjs/core/services/router';
+logger.info('app test okok');
+router.on('/').render('pages/home');
+//# sourceMappingURL=routes.js.map
